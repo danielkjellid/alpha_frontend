f409a90239848b642964bf4c32e322d9b656912a
danielkjellid <daniel.kjellid@gmail.com>
2020-08-09 16:59:17 +0200

    chore: documentation of code related to beforeenter hook in router

--- src/router/index.js
+++ src/router/index.js
@@ -28,12 +28,17 @@ router.beforeEach(async (routeTo, routeFrom, next) => {
   
   const currentUser = store.state.users.currentUser
 
+  // if auth isn't required for the route, just continue
   if (!authRequired) return next()
 
+  // if the user has to be authenticated
   if (authRequired) {
-    if (currentUser && currentUser.is_authenticated) { 
+    // check is user is not null and that the user is authenticated
+    if (currentUser && currentUser.is_authenticated) {
+      // if everything passes, just continue
       next()
     } else {
+        // if the user is null or the user isn't authenticated
         // run the fetchCurrentUser action in store
         // this is in case of the route being accessed before the init action is run, and the currentUser state is null
         await store.dispatch('users/fetchCurrentUser')
@@ -45,20 +50,30 @@ router.beforeEach(async (routeTo, routeFrom, next) => {
             if (!user.is_authenticated) {
               window.location.href = '/bruker/logg-inn/'
             } else {
+              // if the user is authenticated, just continue
               next()
             }
           })
     }
   }
 
+  // if the user has to be staff
   if (staffRequired) {
+    // check is user is not null and that the user is authenticated
     if (currentUser && currentUser.is_authenticated) {
+      // check if the user is staff
       if (currentUser.is_staff) {
+        // if everything passes, just continue
         next()
       } else {
+        // if the user is authenticated, but not staff
+        // redirect to 404 - Not found
         next({ path: '/404' })
       }
     } else {
+      // if the user is null or the user isn't authenticated
+      // run the fetchCurrentUser action in store
+      // this is in case of the route being accessed before the init action is run, and the currentUser state is null
       await store.dispatch('users/fetchCurrentUser')
         .then(() => {
           // when the action is run, get the object through the getCurrentUser getter
@@ -67,9 +82,11 @@ router.beforeEach(async (routeTo, routeFrom, next) => {
           // if the user is fetched from the api, and still not authenticated, redirect to login
           if (!user.is_authenticated) {
             window.location.href = '/bruker/logg-inn/'
+          // if the user is fetched and authenticated, but not staff, redirect to 404
           } else if (!user.is_staff) {
             next({ path: '/404' })
           } else {
+            // if everything passes, just continue
             next()
           }
         })
