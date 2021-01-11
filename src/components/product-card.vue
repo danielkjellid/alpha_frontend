412673decf76817d13b0291c20f2f6a4d5c1dcb8
danielkjellid <daniel.kjellid@gmail.com>
2021-01-11 16:44:30 +0100

    refactor: changed representation of prices

--- src/components/product-card.vue
+++ src/components/product-card.vue
@@ -16,8 +16,8 @@
       </div>
       <div class="product-info pt-4 pb-1">
         <div v-if="!showVariants" class="flex items-center justify-between">
-          <p class="w-3/5 text-base font-medium text-gray-900 truncate">{{ product.name }}</p>
-          <p class="w-2/5 text-sm font-medium text-gray-900">kr {{ product.gross_price | formatPrice }} <span class="text-gray-500">{{ product.unit }}</span></p>
+          <p class="w-1/2 text-base font-medium text-gray-900 truncate">{{ product.name }}</p>
+          <p class="w-1/2 text-sm font-medium text-gray-900">fra {{ product.gross_price | formatPrice }} <span class="text-gray-500">{{ product.unit }}</span></p>
           <!-- TODO: add logic for discounted products -->
           <!-- <p class="text-sm font-medium text-gray-900">
             <span class="text-gray-500"><strike>kr 499,00</strike></span> - old price
