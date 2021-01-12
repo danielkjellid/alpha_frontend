06fe813c55eb0a9f9ce3a1ef4224c7b52af05a22
danielkjellid <daniel.kjellid@gmail.com>
2021-01-12 20:36:09 +0100

    refactor: integrated CTA section in kitchen list with a introduction video

--- src/views/inventory-kitchen-list.vue
+++ src/views/inventory-kitchen-list.vue
@@ -6,27 +6,38 @@
       <!-- site product content -->
       <div class="container max-w-screen-xl min-h-screen mx-auto">
         <BaseBreadcrumbs :breadcrumbs="breadcrumbs" class="xl:px-0 px-4 py-8" />
-        <div class="bg-gray-50 w-full">
-          <div class="sm:px-6 lg:px-8 max-w-2xl px-4 py-8 mx-auto text-center">
-            <h2 class="sm:text-4xl sm:leading-10 text-2xl font-extrabold leading-9 text-gray-900">
-              En verden av muligheter, skreddersydd for deg.
-            </h2>
-            <p class="mt-4 text-lg leading-6 text-gray-600">Vi er stolte samarbeidspartnere med Kjøkkenhuset Lillestrøm, som tilbyr et hav av mulige løsninger fra JKE Design, skreddersydd dine behov.</p>
-            <a href="https://jke-design.com/no/booking/velg-tidspunkt/?sId=2784" target="_blank" rel="noopener noreferrer" class="hover:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring-gray active:bg-gray-900 inline-flex items-center flex-shrink-0 px-4 py-2 mt-8 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md">Bestill tegnetime hos oss i dag!</a>
+        <div class="xl:px-0 px-4">
+          <div class="bg-gray-50 relative w-full">
+            <div class="sm:h-72 md:absolute md:right-0 md:h-full md:w-1/2 md:top-0 h-56">
+              <video
+                controls
+                height="2000" 
+                src="https://flishuset.s3.eu-north-1.amazonaws.com/media/kitchens/videos/JKE-Design-production.mp4"
+                class="object-cover w-full h-full"
+              >
+                Sorry, your browser doesn't support embedded videos,
+                but don't worry, you can <a href="https://flishuset.s3.eu-north-1.amazonaws.com/media/kitchens/videos/JKE-Design-production.mp4">download it</a>
+                and watch it with your favorite video player!
+              </video>
+            </div>
+            <div class="max-w-7xl sm:px-6 lg:px-8 lg:py-16 relative px-4 py-12 mx-auto">
+              <div class="md:mr-auto md:w-1/2 md:pr-10">
+                <h2 class="text-base font-semibold tracking-wider text-gray-500 uppercase">
+                  Stilrene nordiske kjøkken
+                </h2>
+                <p class="sm:text-4xl mt-2 text-3xl font-extrabold tracking-tight text-gray-900">
+                  En verden av muligheter, skreddersydd for deg.
+                </p>
+                <p class="mt-3 text-lg text-gray-600">
+                  Vi er stolte samarbeidspartnere med Kjøkkenhuset Lillestrøm, som tilbyr et hav av mulige løsninger fra JKE Design, skreddersydd dine behov.
+                </p>
+                <div class="mt-2">
+                  <a href="https://jke-design.com/no/booking/velg-tidspunkt/?sId=2784" target="_blank" rel="noopener noreferrer" class="hover:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring-gray active:bg-gray-900 inline-flex items-center flex-shrink-0 px-4 py-2 mt-8 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md">Bestill tegnetime hos oss i dag!</a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <section class="px-5 mt-5">
-          <video
-            controls
-            height="2000" 
-            src="https://flishuset.s3.eu-north-1.amazonaws.com/media/kitchens/videos/JKE-Design-production.mp4"
-            class="mt-4"
-          >
-            Sorry, your browser doesn't support embedded videos,
-            but don't worry, you can <a href="https://flishuset.s3.eu-north-1.amazonaws.com/media/kitchens/videos/JKE-Design-production.mp4">download it</a>
-            and watch it with your favorite video player!
-          </video>
-        </section>
         <section class="mt-5">
           <div v-if="loaded">
             <div 
